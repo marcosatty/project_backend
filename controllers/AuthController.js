0a7ce13bf1316860b9741aa1c4a7f23e51fbd45b
marcosatty <marquinhoanjos51@gmail.com>
2024-04-12 23:10:12 -0300

    att login cadastro

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,13 +5,51 @@ const User = require('../models/User')
 const bcrypt = require('bcryptjs')
 
 
-// ou module.exports = class AuthController
+//module.exports = class UserController{
 module.exports = class AuthController{
     
     static login(req, res){
         res.render('auth/login')
     }
 
+    static async loginPost(req, res) {
+        const { email, password } = req.body
+
+        // Checagem se o usuario existe
+
+        const user = await User.findOne({ where: { email: email }})
+    
+        if(!user){
+
+        req.flash('message', 'Não existe cadastro com esse E-mail, Por favor realizer seu cadastro')
+        res.render('auth/login')
+            return
+
+        }
+
+        // check Match para senha incorreta apos realizada a busca no banco 
+
+        const passwordMatch = bcrypt.compareSync(password, user.password)
+
+        if(!passwordMatch) {
+        req.flash('message', 'Sua Senha esta incorreta, Tente novamente')
+        res.render('auth/login')
+            return
+        }
+
+    //inicializar a sessão do usurario
+
+        req.session.userid = user.id
+        //caso eu queira tirar a mensagem apos realizado o login, so remover o req flash
+        req.flash('message', 'Autenticação Realizada com Exito!')
+  
+        req.session.save(() => {
+            res.redirect('/')
+        })
+  
+
+
+    }
 
     static cadastrar(req, res){
         res.render('auth/cadastrar')
@@ -27,23 +65,21 @@ module.exports = class AuthController{
 
     const { name, email, telefone, password, confirmpassword } = req.body
 
-  
-    
 
     //validação da password
-   //if (password != confirmpassword) {
-       // req.flash('message', 'As senhas não conferem, tente novamente!')
-       //res.render('auth/register')
+   if (password != confirmpassword) {
+       req.flash('message', 'As senhas não conferem, tente novamente!')
+       res.render('auth/cadastrar')
   
-        //return
-      //}
+        return
+      }
 
     //check if se o usuario existe
     const checkIfUserExists = await User.findOne({ where: { email: email }})
     
     if(checkIfUserExists) {
     //esse é pra pegar a estelização do css da main
-        req.flash('message', 'E-mail já está em uso!')
+        req.flash('message', 'E-mail já Cadastrado!')
         res.render('auth/cadastrar')
         
         return
@@ -62,27 +98,39 @@ module.exports = class AuthController{
 
     const salt = bcrypt.genSaltSync(10)
     //gerando hashed da senha
-    
     const hashedPassword = bcrypt.hashSync(password, salt)
 
     const user ={
         name,
         email,
         telefone,
-        password: hashedPassword,
+        password: hashedPassword
     }
 
-    try{
-        await User.create(user)
-
+    try{ //CRIANDO CONSTANTE PARA SESSAÃO
+        const createdUser = await User.create(user)
+        
+        //inicializar a sessão do usurario
+        req.session.userid = createdUser.id
+        //esse message é a estilização do css
         req.flash('message', 'Cadastro Realizado com Sucesso!')
-        res.redirect('/')
 
+        req.session.save(() => {
+            res.redirect('/')
+        })
+
+    
     } catch (err) {
 
         console.log(err)
     }
 
 }
+    //criação do logout
+
+    static logout (req, res) {
+        req.session.destroy()
+        res.redirect('/login')
+    }
 
 }
--- routes/authRouters.js
+++ routes/authRouters.js
@@ -3,12 +3,23 @@ const router = express.Router()
 const AuthController = require('../controllers/AuthController')
 //colocando o controllers
 //criação de uma class
+
+//LOGIN
 router.get('/login', AuthController.login)
 
+router.post('/login', AuthController.loginPost)
+
+//CADASTRAR
 router.get('/cadastrar', AuthController.cadastrar)
 
 router.post('/cadastrar', AuthController.cadastrarPost)
 
+// logout do usuario
+
+router.get('/logout', AuthController.logout)
+
+
+//SOBRE
 router.get('/sobre', AuthController.sobre)
 
 module.exports = router
